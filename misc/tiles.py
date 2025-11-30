722ef175d36404f5e0acefd21dae0503c5b3e6e7
tstr92 <tstrahlheim@gmail.com>
2025-11-30 23:42:55 +0100

    variable naming

--- misc/tiles.py
+++ misc/tiles.py
@@ -6,7 +6,7 @@ palette = [(255,255,255), (170,170,170), (85,85,85), (0,0,0)]
 # palette = [(0x26,0x34,0x23), (0x53,0x70,0x2f), (0xa6,0xb6,0x3d), (0xf1,0xf3,0xc0)]
 
 def main():
-    tile = []
+    tile_pixels = []
     for i in range(0, len(data), 2):
         lsb = data[i + 0]
         msb = data[i + 1]
@@ -14,7 +14,7 @@ def main():
             t = 0
             t += 1 if (0 != lsb & (1<<j)) else 0
             t += 2 if (0 != msb & (1<<j)) else 0
-            tile.append(t)
+            tile_pixels.append(t)
         #     print(t, end="")
         # print()
     
@@ -24,7 +24,7 @@ def main():
 # Set the pixel colors from top-left to bottom-right
     for y in range(8):
         for x in range(8):
-            pixels[x, y] = palette[tile[y*8+x]]
+            pixels[x, y] = palette[tile_pixels[y*8+x]]
 
     img = img.resize((800, 800), Image.NEAREST)
     img.show()
